1edc0f214827a8852d14550e66d6c8e3f9fdbbcd
mahamadouf <123761069+mahamadouf@users.noreply.github.com>
2024-09-20 18:40:05 +0000

    Answered

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Version Control Assessment
 // 1. Explain what version control is.
 // Write your answer as a string and store it in the variable below:
-let versionControlExplanation = "";
+let versionControlExplanation = "Version Control is a tool that helps track changes and checkpoints in your projects";
 
 console.log("What is version control?");
 console.log(versionControlExplanation); 
@@ -15,14 +15,14 @@ console.log(versionControlExplanation);
 
 // 2. Explain what a Git repository is.
 // Write your answer as a string and store it in the variable below:
-let gitRepoExplanation = "";
+let gitRepoExplanation = "Git repository is a tool that creates ormanages version control";
 
 console.log("What is a Git repository?");
 console.log(gitRepoExplanation); 
 
 // 3. Name the benefits of using version control.
 // Write your answer as an array of strings below, with each string being a benefit.
-let versionControlBenefits = [];
+let versionControlBenefits = ["Efficient", "Less Buggy", "Smooth"];
 
 console.log("What are the benefits of using version control?");
 console.log(versionControlBenefits); 
@@ -34,7 +34,7 @@ console.log(versionControlBenefits);
 
 // 4. Explain the steps to pushing changes in Git.
 // Write your answer as an array of steps below, with each step being a string.
-let stepsToPushChanges = [];
+let stepsToPushChanges = ["Make Changes: git status", "Stage the change: git add *","Commit the change: git commit -m message(in quotation marks)","Push the commit(s): git push"];
 
 console.log("What are the steps to pushing changes in Git?");
 console.log(stepsToPushChanges); 
